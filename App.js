38baf17099630d78a031dc4d0f2d8835387257dd
Mohamed Belhedi <mbelhedi@outlook.com>
2023-02-20 14:38:12 +0100

    grammarlyapp update v-1

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import { GrammarlyEditorPlugin } from '@grammarly/editor-sdk-react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextBase,TextInput} from 'react-native';
+import React,{useEffect,useState} from 'react';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Grammer Correction App</Text>
+     <GrammarlyEditorPlugin clientId='client_Dn1ZCRrauR12cEkAcUgXaj'>
+      
+    <TextInput multiline    style={
+      {
+        maxHeight: 200,
+        paddingBottom: 30,
+      }
+      
+    } 
+    onblur={onblur}
+    
+    />
+    
+      
+  </GrammarlyEditorPlugin>
+      
       <StatusBar style="auto" />
     </View>
   );
